cc8c90c4a24efbae126db9b87c920889a22474f4
Pravin Jadhav <83411817+pravinpadmakarjadhav@users.noreply.github.com>
2021-12-06 16:00:21 +0530

    updat

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <Link href='/'>
           <a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
-            <Image src='/images/logo.png' width={40} height={40} alt='logo' />
+            <Image src='/images/cloud-server.png'  width={40} height={40} alt='logo' />
             <span className='ml-3 text-xl'>Cloudcafe</span>
           </a>
         </Link>
